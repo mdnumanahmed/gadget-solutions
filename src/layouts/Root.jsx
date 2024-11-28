10661b83d4bf05ad9727de6bb4ac5a57c6f4580c
mdnumanahmed <mnumansyl@gmail.com>
2024-11-28 21:12:42 +0600

    created re-useable PageTItle Component and added ProductDetails page

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,9 +1,12 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 const Root = () => {
+  const location = useLocation();
   return (
     <div>
+      {location.pathname !== "/" && <Navbar />}
       <Outlet />
       <Footer />
     </div>
---
+++ src/pages/ProductDetails.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import PageTitle from "../components/PageTitle";
+
+const ProductDetails = () => {
+  return (
+    <div>
+      <PageTitle
+        title={"Product Details"}
+        subTitle={
+          "Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!"
+        }
+      />
+    </div>
+  );
+};
+
+export default ProductDetails;
