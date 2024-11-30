d70b2b463cb2c88c4ceeb1a2634c1634643d7e96
mdnumanahmed <mnumansyl@gmail.com>
2024-11-30 22:30:42 +0600

    created AuthContext, and AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes/Route.jsx";
 import DataProvider from "./providers/DataProvider.jsx";
 import { Toaster } from "react-hot-toast";
+import AuthProvider from "./providers/AuthProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <DataProvider>
-      <RouterProvider router={router} />
-      <Toaster />
-    </DataProvider>
+    <AuthProvider>
+      <DataProvider>
+        <RouterProvider router={router} />
+        <Toaster />
+      </DataProvider>
+    </AuthProvider>
   </StrictMode>
 );
